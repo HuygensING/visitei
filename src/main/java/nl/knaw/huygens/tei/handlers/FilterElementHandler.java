d433dff0cca8095a0e5fc5dfa74fcdfb814a7c71
Bram Buitendijk as svn <bram.buitendijk@huygens.knaw.nl>
2011-11-26 16:06:41 +0000

    [WR] Generalized FilterElementHandler. Upgraded to Google Guava 10.0.1

--- src/main/java/nl/knaw/huygens/tei/handlers/FilterElementHandler.java
+++ src/main/java/nl/knaw/huygens/tei/handlers/FilterElementHandler.java
@@ -1,22 +1,22 @@
 package nl.knaw.huygens.tei.handlers;
 
+import nl.knaw.huygens.tei.Context;
 import nl.knaw.huygens.tei.Element;
 import nl.knaw.huygens.tei.ElementHandler;
 import nl.knaw.huygens.tei.Traversal;
-import nl.knaw.huygens.tei.XmlContext;
 
 /**
  * Ignores an element and its children.
  */
-public class FilterElementHandler implements ElementHandler<XmlContext> {
+public class FilterElementHandler<T extends Context> implements ElementHandler<T> {
 
   @Override
-  public Traversal enterElement(Element element, XmlContext context) {
+  public Traversal enterElement(Element element, T context) {
     return Traversal.STOP;
   }
 
   @Override
-  public Traversal leaveElement(Element element, XmlContext context) {
+  public Traversal leaveElement(Element element, T context) {
     return Traversal.NEXT;
   }
 
