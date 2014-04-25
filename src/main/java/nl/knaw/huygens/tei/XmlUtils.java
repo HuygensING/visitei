739e3e300d6522e863eb415551397ddba2377466
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2014-04-25 13:45:19 +0000

    preserve attribute order in element

--- src/main/java/nl/knaw/huygens/tei/XmlUtils.java
+++ src/main/java/nl/knaw/huygens/tei/XmlUtils.java
@@ -9,17 +9,20 @@ import com.google.common.collect.Maps;
 public class XmlUtils {
 
   public static Map<String, String> newAttributes() {
-    return Maps.newTreeMap();
+    //    return Maps.newTreeMap();
+    return Maps.newLinkedHashMap();
   }
 
   public static Map<String, String> newAttributes(String key, String value) {
-    Map<String, String> map = Maps.newTreeMap();
+    //    Map<String, String> map = Maps.newTreeMap();
+    Map<String, String> map = Maps.newLinkedHashMap();
     map.put(key, value);
     return map;
   }
 
   public static Map<String, String> convertSAXAttributes(Attributes attributes, boolean useQualifiedName) {
-    Map<String, String> map = Maps.newTreeMap();
+    //    Map<String, String> map = Maps.newTreeMap();
+    Map<String, String> map = Maps.newLinkedHashMap();
     for (int i = 0; i < attributes.getLength(); i++) {
       String key = useQualifiedName ? attributes.getQName(i) : attributes.getLocalName(i);
       String value = attributes.getValue(i);
