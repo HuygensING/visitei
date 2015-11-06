46656cb6ea6961c25f2cbdf6f1d2e82f21746b6f
Bram Buitendijk as svn <bram.buitendijk@huygens.knaw.nl>
2015-11-06 10:20:41 +0000

    refactoring

--- src/main/java/nl/knaw/huygens/tei/XmlUtils.java
+++ src/main/java/nl/knaw/huygens/tei/XmlUtils.java
@@ -1,28 +1,24 @@
 package nl.knaw.huygens.tei;
 
 import java.util.Map;
-
 import org.xml.sax.Attributes;
-
 import com.google.common.collect.Maps;
 
 public class XmlUtils {
 
   public static Map<String, String> newAttributes() {
-    //    return Maps.newTreeMap();
+    // use LinkedHashMap to preserve attribute order
     return Maps.newLinkedHashMap();
   }
 
   public static Map<String, String> newAttributes(String key, String value) {
-    //    Map<String, String> map = Maps.newTreeMap();
-    Map<String, String> map = Maps.newLinkedHashMap();
+    Map<String, String> map = newAttributes();
     map.put(key, value);
     return map;
   }
 
   public static Map<String, String> convertSAXAttributes(Attributes attributes, boolean useQualifiedName) {
-    //    Map<String, String> map = Maps.newTreeMap();
-    Map<String, String> map = Maps.newLinkedHashMap();
+    Map<String, String> map = newAttributes();
     for (int i = 0; i < attributes.getLength(); i++) {
       String key = useQualifiedName ? attributes.getQName(i) : attributes.getLocalName(i);
       String value = attributes.getValue(i);
